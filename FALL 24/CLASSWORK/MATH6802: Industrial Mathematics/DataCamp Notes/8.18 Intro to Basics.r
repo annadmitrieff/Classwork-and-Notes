6e58f26962fb9713cf2ba1c51f46b634320b3cdb
annie dmitrieff <142821835+annadmitrieff@users.noreply.github.com>
2024-08-19 08:22:41 -0400

    Add files via upload

---
+++ FALL 24/CLASSWORK/MATH6802: Industrial Mathematics/DataCamp Notes/8.18 Intro to Basics.r
@@ -0,0 +1,27 @@
+# 08/18 Intro to Basics
+
+# Assigning Variables:
+a <- 42 # Assigns the integer ('numeric') to a
+a # Prints a
+
+# Basic Data Types:
+numeric <- 4.5
+integer <- 4 # Integers are also considered 'numeric'.
+boolean1 <- TRUE # Boolean values (TRUE and FALSE) are called 'logical'
+boolean2 <- FALSE
+characters <- "hello world" # Text, or strings, indicated by quotation marks, are called 'characters'.
+
+# class() is a function that outputs the data type of an input:
+class(42)
+
+# Basic Operations:
+x <- 1
+y <- 2
+
+x + y # Addition
+x - y # Subtraction
+x * y # Multiplication
+x / y # Division
+x ^ y # Exponentiation
+x %% y # Modulus
+x %/%y # Integer Division (Floor Function)
